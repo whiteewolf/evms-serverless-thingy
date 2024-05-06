3e955f27b590f765507acdd69e77fcf78a68b581
Mizchiev Dev <arog0660@gmail.com>
2024-05-06 22:33:18 +0300

    Update receive-data.js

--- functions/api/receive-data.js
+++ functions/api/receive-data.js
@@ -6,7 +6,7 @@ export async function onRequestGet(context) {
 
   const data = await Promise.all(
     keys.keys.map(async (key) => {
-      const value = await env.MY_KV.get(key.name);
+      // const value = await env.MY_KV.get(key.name);
       return { key: key.name, value: value ? JSON.parse(value) : null };
     })
   );
